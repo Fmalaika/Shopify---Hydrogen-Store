eac8fb1ec310bf4bd7cf42fc82cf35654122abe3
Malaika <malaikafernandes25@gmail.com>
2022-07-15 18:44:38 +0530

    initial commit

---
+++ hydrogen.config.js
@@ -0,0 +1,20 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    // storeDomain: 'hydrogen-preview.myshopify.com',
+    // storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'malaika-storeeeee.myshopify.com/',
+    storefrontToken: '0f86d8ef94e003be0408926bb7542387',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
